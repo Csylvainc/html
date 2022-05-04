d1ac7a7dc9f97f37f6346e2d784656a1ac7a612f
Csylvainc <81378896+Csylvainc@users.noreply.github.com>
2022-05-04 17:37:09 +0200

    with logo in menu and btn close

--- menu.js
+++ menu.js
@@ -3,5 +3,11 @@ document.querySelector('#hamburger').onclick = function() {
     document.querySelector('#menu').classList.toggle('menuVisible')
 
 };
+document.querySelector('.close-btn').onclick = function() {
+    document.querySelector('#menu').classList.toggle('menuCache')
+    document.querySelector('#menu').classList.toggle('menuVisible')
+
+};
+
 
 
